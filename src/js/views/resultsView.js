e08eefd30a27e039f89520dcabf153e782f6edd6
DAI <senseye935@gmail.com>
2022-05-06 21:35:49 +0800

    Creat Pagination

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,5 +1,5 @@
-import icons from 'url:../../img/icons.svg';
 import View from './View.js';
+import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
