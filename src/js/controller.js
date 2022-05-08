1d24db5499747c06549b2b9cc830b2959e94d12e
DAI <senseye935@gmail.com>
2022-05-08 15:44:35 +0800

    Imports in helpers.js and controller.js.

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,7 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
+import { async } from 'regenerator-runtime';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
